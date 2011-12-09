8b197b403e23d5d7fa1c96d0dd4891800b430317
Rupert Swarbrick <rswarbrick@gmail.com>
2011-12-09 13:26:04 +0000

    Actually run the new EDN source.

--- atonews.lisp
+++ atonews.lisp
@@ -3,6 +3,9 @@
 (defun run ()
   (with-lock-file
     (let ((server (make-server)))
-      (let ((source (make-instance 'maxim-appnotes-ns))
-            (group (make-group "local.electronics.maxim.app-notes" :server server)))
-        (update-news-source source group)))))
+      (map nil
+           #'update-news-source
+           (list (make-instance 'maxim-appnotes-ns)
+                 (make-instance 'edn-design-ideas-ns))
+           (list (make-group "local.electronics.maxim.app-notes" :server server)
+                 (make-group "local.electronics.edn.design-ideas" :server server))))))
