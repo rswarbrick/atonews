cb08fd18c4fd2db3dbcba07c44df2c652884325f
Rupert Swarbrick <rswarbrick@gmail.com>
2012-06-19 14:56:01 +0100

    Deal gracefully when an RSS channel has no valid link.

--- rss.lisp
+++ rss.lisp
@@ -16,14 +16,14 @@ the link, so cache this here in order to be able to use it when making the full
 message. This doesn't inherit from HTTP-MESSAGE-FRAGMENT, because the meaning of
 URL is rather different."))
 
-(defun rss-uri-to-message-id (uri referrer)
+(defun rss-uri-to-message-id (uri host)
   (format nil "<~A@~A>"
           (cl-base64:string-to-base64-string (sb-md5:md5sum-string uri))
-          (puri:uri-host (puri:parse-uri referrer))))
+          host))
 
-(defun rss-make-message-fragment (item channel from-address)
+(defun rss-make-message-fragment (item channel from-address host)
   (let ((frag (make-message-fragment
-               (rss-uri-to-message-id (rss:link item) (rss:link channel))
+               (rss-uri-to-message-id (rss:link item) host)
                (rss:title item)
                from-address
                :date (or (rss:pub-date item) (rss:pub-date channel))
@@ -32,13 +32,21 @@ URL is rather different."))
     (setf (slot-value frag 'description) (rss:description item))
     frag))
 
+(defmethod get-header-data :around ((ns rss-source))
+  (let ((drakma:*text-content-types*
+         (cons '("application" . "rss+xml") drakma:*text-content-types*)))
+    (call-next-method)))
+
 (defmethod find-message-fragments ((source rss-source) contents)
-  (let* ((channel (rss:parse-rss-stream contents :err t :strict? t))
-         (from-address (format nil "dev.null@~A"
-                               (puri:uri-host (puri:parse-uri
-                                               (rss:link channel))))))
+  (let* ((channel (rss:parse-rss-stream contents :err t :strict? nil))
+         (host (puri:uri-host
+                (puri:parse-uri (if (< 0 (length (rss:link channel)))
+                                    (rss:link channel) (list-url source)))))
+         (from-address (format nil "dev.null@~A" host)))
+    (print (if (< 0 (length (rss:link channel)))
+               (rss:link channel) (list-url source)))
     (mapcar (lambda (item)
-              (rss-make-message-fragment item channel from-address))
+              (rss-make-message-fragment item channel from-address host))
             (rss:items channel))))
 
 (defmethod expand-message-fragment ((source rss-source)
